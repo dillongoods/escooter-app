7d62f6156fc039ffc103c313eed3beaf355ed6ef
Jordan Bushnell <jordan.bushnell@outlook.com>
2022-02-22 13:44:11 +0000

    Setup SQLAlchemy

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,12 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.debug = True
 
-from app import views
+app.config.from_object('config')
+
+# Create instance of the database object
+db = SQLAlchemy(app)
+
+from app import views, models
