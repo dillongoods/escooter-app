9825d47beab86c992ad2ddd8c5bc7b2e0ed56aef
Jordan Bushnell <jordan.bushnell@outlook.com>
2022-02-23 23:08:25 +0000

    Add layouts and auth pages using flask-security

--- app/__init__.py
+++ app/__init__.py
@@ -2,11 +2,14 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_security import Security, SQLAlchemyUserDatastore
+import logging
 
 app = Flask(__name__)
 app.debug = True
 
 app.config.from_object('config')
+
+logging.basicConfig(level=logging.DEBUG)
 
 # Create instance of the database object
 db = SQLAlchemy(app)
@@ -18,7 +21,7 @@ from app import views, models
 user_datastore = SQLAlchemyUserDatastore(db, models.User, models.Role)
 security = Security(app, user_datastore)
 
-# Create a user to test with
+# # Create a user to test with
 # @app.before_first_request
 # def create_user():
 #     db.create_all()
--- app/views.py
+++ app/views.py
@@ -1,71 +1,23 @@
-from flask import render_template, flash, request, session, redirect, db
-from app import app, models
-from .forms import RegistrationForm, LogIn, AddItem, ChangePassword, StoreCardDetails, CreateBooking
+from flask import session, render_template
+from app import app, models, db
+from flask_security import login_required, current_user
 
-def signedIn():
-    return session.get('loggedIn') == True
+# Wrapper for render_template to always include whether user is authenticated
+def render_template_auth(template, **template_vars):
+    return render_template(
+        template, 
+        authenticated = current_user.is_authenticated,
+        **template_vars
+    )
 
-def unsignedIn():
-    return session.get('loggedIn') is None or session.get('loggedIn') == False
-
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template_auth('not_found.html')
 
 @app.route('/')
 def index():
+    # return render_template_auth('index.html')
+
     roles = models.Role.query.all()
     users = models.User.query.all()
-    return ' '.join([role.name for role in roles])
-
-@app.route('/log_in', methods=['GET', 'POST'])
-def log_in():
-
-    if signedIn() == True:
-        #logging.info('User doesnt need to log in as theyre already logged in')
-        return redirect('/')
-
-    form = LogIn()
-    if form.validate_on_submit():
-        existingUser = models.User.query.filter_by(email=form.email.data).first()
-
-        if existingUser is None:
-            flash('%s is not registered in our database. Register first to continue'%(form.email.data))
-            app.logger.info('%s failed to log in', form.email.data)
-        elif existingUser.password != form.password.data:
-            flash('Incorrect password')
-            app.logger.info('%s failed to log in', existingUser.email)
-        else:
-            session['loggedIn'] = True
-            session['id'] = existingUser.id
-            session['email'] = existingUser.email
-            app.logger.info('%s logged in successfully', existingUser.email)
-            return redirect('/')
-
-    return render_template('log_in.html', title='Log in', form=form)
-
-@app.route('/logout')
-def logout():
-    session.pop('loggedIn', None)
-    #logging.info('user logged out')
-    return redirect('/')
-
-@app.route('/change_password', methods=['GET', 'POST'])
-def change_password():
-    form = ChangePassword()
-
-    if form.validate_on_submit():
-
-        userid = session.get('id')
-        user = models.User.query.filter_by(id=userid).first()
-
-        if form.newPassword.data != form.repeatNew.data:
-            flash("Passwords dont match, please try again")
-        elif (user.password != form.oldPassword.data):
-            #logging.basicConfig(format='%(asctime)s %(message)s')
-            #logging.info(': failed change password attempt')
-            flash("Old password doesn't match password on account")
-        elif form.newPassword.data == form.repeatNew.data and user.password == form.oldPassword.data:
-            user.password = form.newPassword.data
-            db.session.commit()
-            #app.logger.info('%s changed password', user.email)
-            flash("Password changed successfully")
-
-    return render_template('change_password.html', form=form)
+    return render_template_auth('index.html', roles = roles, users = users)
