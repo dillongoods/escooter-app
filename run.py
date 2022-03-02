b3ffee2c4e502a49efe8478d228001df6e250e17
Canaan Weise <ed193cw@leeds.ac.uk>
2022-03-02 12:30:32 +0000

    Partial implementation of designs on base page.

--- run.py
+++ run.py
@@ -1,4 +1,8 @@
-from app import app
+from app import app, redirect, url_for,Flask,render_template
+
+@app.route("/")
+def home():
+    return render_template("index.html")
 
 if __name__ == '__main__':
     app.run(debug=True)
