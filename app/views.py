3ed2bd45f58713657027e1fea34313b36ff056c3
JokubasTolocka <jokubas.tj@gmail.com>
2022-03-09 19:53:31 +0000

    format code

--- app/views.py
+++ app/views.py
@@ -7,6 +7,8 @@ import datetime
 HIRE_CHOICES = [('1', '1 hr'), ('2', '4 hrs'), ('3', '1 day'), ('4', '1 week')]
 
 # Wrapper for render_template to always include whether user is authenticated
+
+
 def render_template_auth(template, **template_vars):
     return render_template(
         template,
@@ -15,9 +17,12 @@ def render_template_auth(template, **template_vars):
     )
 
 # Manager route
+
+
 def checkAndRedirectManager():
     if current_user.has_role('manager'):
         return render_template_auth('manager/index.html')
+
 
 @app.errorhandler(404)
 def page_not_found(e):
@@ -43,37 +48,40 @@ def index():
 
     # return render_template_auth('index.html', form=allLocations)
 
+
 @app.route('/account', methods=['GET'])
 @auth_required()
 def my_account():
 
-    #for users to view their account details
+    # for users to view their account details
 
     user_email = current_user.email
 
     user = models.User.query.filter_by(email=user_email).first()
-    details = models.BankDetails.query.filter_by(id=user.bank_details_id).first()
+    details = models.BankDetails.query.filter_by(
+        id=user.bank_details_id).first()
 
-    return render_template_auth('my_account.html', title = 'My Account', user=user, card_details=details)
+    return render_template_auth('my_account.html', title='My Account', user=user, card_details=details)
+
 
 @app.route('/account/bank_details', methods=['GET', 'POST'])
 @auth_required()
 def bank_details():
 
-    #for users to store their bank details
+    # for users to store their bank details
     #userid = session.get('id')
     #user = models.User.query.filter_by(id=userid).first()
 
-    #if request.method == 'POST':
+    # if request.method == 'POST':
     details_form = StoreCardDetailsForm()
 
     if details_form.validate_on_submit():
         details = models.BankDetails(
-            name = details_form.name.data,
-            accountNo = details_form.accountNo.data,
-            sortCode = details_form.sortCode.data,
-            expiry = details_form.expiry.data,
-            cvc = details_form.cvc.data
+            name=details_form.name.data,
+            accountNo=details_form.accountNo.data,
+            sortCode=details_form.sortCode.data,
+            expiry=details_form.expiry.data,
+            cvc=details_form.cvc.data
         )
 
         db.session.add(details)
@@ -92,7 +100,7 @@ def bank_details():
         #     #relationship
         #     db.session.commit()
 
-    return render_template_auth('bank_details.html', title = 'My Account', form=details_form)
+    return render_template_auth('bank_details.html', title='My Account', form=details_form)
 
 
 # Hire a scooter
@@ -110,18 +118,19 @@ def hireScooter():
 
     user_email = current_user.email
     user = models.User.query.filter_by(email=user_email).first()
-    details = models.BankDetails.query.filter_by(id=user.bank_details_id).first()
+    details = models.BankDetails.query.filter_by(
+        id=user.bank_details_id).first()
     details_form = StoreCardDetailsForm()
 
     accountNo = str(details.accountNo)[-4:] if details else None
 
     if details_form.validate_on_submit():
         details = models.BankDetails(
-            name = details_form.name.data,
-            accountNo = details_form.accountNo.data,
-            sortCode = details_form.sortCode.data,
-            expiry = details_form.expiry.data,
-            cvc = details_form.cvc.data
+            name=details_form.name.data,
+            accountNo=details_form.accountNo.data,
+            sortCode=details_form.sortCode.data,
+            expiry=details_form.expiry.data,
+            cvc=details_form.cvc.data
         )
 
         db.session.add(details)
@@ -132,15 +141,14 @@ def hireScooter():
         u.bank_details_id = details.id
         db.session.commit()
 
-
     #data = json.load(request.data)
     #duration = data.get("durationSelect")
-
-
 
     return render_template_auth('hireScooter.html', scooter=scooter, location=location, allLocations=allLocations, durationOptions=HIRE_CHOICES, has_card_details=details is not None, details_form=details_form, accountNo=accountNo)
 
 # Perform the hiring
+
+
 @app.route('/confirmHire')
 def confirmHire():
     pickupLocationId = request.args.get('pickupLocationId')
@@ -149,35 +157,27 @@ def confirmHire():
     cost = request.args.get('cost')
     scooterId = request.args.get('scooterId')
 
-    print(pickupLocationId, dropoffLocationName, durationInHours, cost, scooterId)
+    print(pickupLocationId, dropoffLocationName,
+          durationInHours, cost, scooterId)
 
-    dropoffLocationId = models.Location.query.filter_by(name=dropOffLocationName).first()
+    dropoffLocationId = models.Location.query.filter_by(
+        name=dropoffLocationName).first()
 
-    booking = Booking(user_id=current_user.id, scooter_id=scooterId, price=cost, length=durationInHours, pickup=pickupLocationId, dropoff=dropoffLocationId)
+    booking = models.Booking(user_id=current_user.id, scooter_id=scooterId, price=cost,
+                             length=durationInHours, pickup=pickupLocationId, dropoff=dropoffLocationId)
 
     selectedScooter = models.Scooter.query.filter_by(id=scooterId).first()
-    selectedScooter.availability=False
+    selectedScooter.availability = False
 
     db.session.add(booking)
     db.session.commit()
     db.session.flush()
 
-    # class Booking(db.Model):
-    #     __tablename__ = 'bookings'
-    #     id = db.Column(db.Integer(), primary_key=True)
-    #     user_id = db.Column('user_id', db.Integer(), db.ForeignKey('users.id'))
-    #     scooter_id = db.Column('scooter_id', db.Integer(),
-    #                            db.ForeignKey('scooters.id'))
-    #     price = db.Column(db.String(255))
-    #     length = db.Column(db.Integer())
-    #     pickup = db.Column('pickup_location_id', db.Integer(),
-    #                        db.ForeignKey('locations.id'))
-    #     dropoff = db.Column('dropoff_location_id', db.Integer(),
-    #                         db.ForeignKey('locations.id'))
-
     return redirect('/')
 
 # Manager Page
+
+
 @app.route('/manager')
 @roles_required('manager')
 @auth_required()
@@ -188,6 +188,7 @@ def manager():
 
     return render_template_auth('manager/index.html', locations=allLocations, Scooters=allScooters)
 
+
 @app.route('/manager/add-location', methods=['GET', 'POST'])
 @roles_required('manager')
 @auth_required()
@@ -196,16 +197,15 @@ def managerAddLocation():
 
     if form.validate_on_submit():
         newLocation = models.Location(
-            name = form.name.data,
-            x_cord = form.x_cord.data,
-            y_cord = form.y_cord.data
+            name=form.name.data,
+            x_cord=form.x_cord.data,
+            y_cord=form.y_cord.data
         )
 
         db.session.add(newLocation)
         db.session.commit()
 
         return redirect('/manager')
-
 
     return render_template_auth('manager/addLocation.html', form=form, map=map)
 
@@ -219,6 +219,8 @@ def logout():
 # API ---------------------------------------
 
 # Get all locations
+
+
 @app.route('/api/getLocations')
 def getLocations():
     allLocations = models.Location.query.all()
@@ -226,23 +228,28 @@ def getLocations():
     return json.jsonify({'locations': models.Location.serialize_list(allLocations)})
 
 # Get all scooters in a specific location
+
+
 @app.route('/api/getScooters')
 def getScootersInLocation():
     locationName = request.args.get('location')
 
     location = models.Location.query.filter_by(name=locationName).first()
 
-    scootersInLocation = models.Scooter.query.filter_by(location_id=location.id, availability=True).all()
+    scootersInLocation = models.Scooter.query.filter_by(
+        location_id=location.id, availability=True).all()
 
     return json.jsonify({'scooters': models.Scooter.serialize_list(scootersInLocation)})
 
 # Add a scooter to the location
+
+
 @app.route('/api/addScooter')
 def addScooterToLocation():
     locationId = request.args.get('locationId')
 
     newScooter = models.Scooter(
-        location_id = locationId
+        location_id=locationId
     )
 
     db.session.add(newScooter)
